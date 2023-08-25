dc9e1a772d6be9c55ebcedc3647d39ebaaa2c2d5
Atikur Rahman <mratikurrahman@gmail.com>
2023-08-26 03:36:50 +0600

    Initial commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Atikur Rahman on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
