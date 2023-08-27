23ec87e1ac1da170ccd0de54d689c94c6a26ba86
Atikur Rahman <mratikurrahman@gmail.com>
2023-08-27 17:43:12 +0600

    Create the detail view

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Atikur Rahman on 8/27/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
